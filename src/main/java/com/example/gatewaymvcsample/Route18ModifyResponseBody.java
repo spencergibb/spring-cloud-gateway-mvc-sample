1448645fe6550d47f5ede99debe348a0cab59cb5
spencergibb <spencer@gibb.us>
2025-03-28 17:39:37 -0400

    Updates to use new modifyResponseBody() filter

--- src/main/java/com/example/gatewaymvcsample/Route18ModifyResponseBody.java
+++ src/main/java/com/example/gatewaymvcsample/Route18ModifyResponseBody.java
@@ -1,17 +1,11 @@
 package com.example.gatewaymvcsample;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.cloud.gateway.server.mvc.common.MvcUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.AfterFilterFunctions.modifyResponseBody;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -25,22 +19,8 @@ public class Route18ModifyResponseBody {
 		return route("modify_response_body")
 				.GET("/anything/modifyresponsebody", http())
 				.before(new HttpbinUriResolver())
-				.after((request, response) -> {
-					Object o = request.attributes().get(MvcUtils.CLIENT_RESPONSE_INPUT_STREAM_ATTR);
-					if (o instanceof InputStream) {
-						try {
-							byte[] bytes = StreamUtils.copyToByteArray((InputStream) o);
-							String s = new String(bytes, StandardCharsets.UTF_8);
-							String replace = s.replace("fooval", "FOOVAL");
-							ByteArrayInputStream bais = new ByteArrayInputStream(replace.getBytes());
-							request.attributes().put(MvcUtils.CLIENT_RESPONSE_INPUT_STREAM_ATTR, bais);
-						}
-						catch (IOException e) {
-							throw new RuntimeException(e);
-						}
-					}
-					return response;
-				})
+				.after(modifyResponseBody(String.class, String.class, null,
+						(_, _, s) -> s.replace("fooval", "FOOVAL")))
 				.build();
 		// @formatter:on
 	}
